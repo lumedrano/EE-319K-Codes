098f3931afc1821b3ee95153e4b91f872e4fe7eb
Luigi Medrano <59450953+lumedrano@users.noreply.github.com>
2022-12-19 21:15:20 -0600

    Add files via upload

---
+++ Lab8_EE319K/adc.d
@@ -0,0 +1,3 @@
+.\adc.o: ..\inc\ADC.c
+.\adc.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\adc.o: ..\inc\../inc/tm4c123gh6pm.h
---
+++ Lab8_EE319K/lab8.d
@@ -0,0 +1,7 @@
+.\lab8.o: Lab8.c
+.\lab8.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\lab8.o: ../inc/ST7735.h
+.\lab8.o: TExaS.h
+.\lab8.o: ../inc/ADC.h
+.\lab8.o: ../inc/tm4c123gh6pm.h
+.\lab8.o: ../inc/StringConversion.h
